b61dfb0fb7ca8aad2c2925e5d400f17a66268347
Aaron Festinger <aaron.festinger@gmail.com>
2019-09-08 16:09:01 -0400

    Binary search with redundancy removed

---
+++ NYCDSA2.py
@@ -0,0 +1,53 @@
+def binarysearch(min, max):
+
+    if min==max:
+        print("The number is "+str(min))
+        return exit()
+    
+    print('The number is in the range ('+str(min)+','+str(max)+').')
+    mid=int(min+(max-min)/2)
+
+    answer=input("Is the number greater than "+str(mid)+"? Please typ 'y' or 'n'.")
+    if answer=='y':
+        return binarysearch(mid+1, max)
+    elif answer=='n':
+        return binarysearch(min, mid)
+    else:
+        print('error')
+        return exit()
+
+validinput=False
+while not validinput:
+    try:
+        min=int(input("Please give a lower bound."))
+        try:
+            max=int(input("Please give an upper bound."))
+            validinput=True
+        except:
+            print("Invalid upper bound")
+    except:
+        print("Invalid lower bound")
+    
+
+
+binarysearch(min, max)
+exit()
+
+
+
+
+def binarysearch(min, max):
+
+    if min==max:
+        print("The number is "+str(min))
+        return
+
+    mid=int((max-min)/2)
+
+    print("Is the number greater than "+str(mid)+"?\n Please typ 'y' or 'n'.")
+    answer=input()
+    if answer=='y':
+        return binarysearch(mid, max)
+    elif answer=='n':
+        return binarysearch(min, mid)
+
