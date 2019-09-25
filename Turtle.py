37e5eb7498b679506504f1c74e150d8751390c3b
Aaron Festinger <aaron.festinger@gmail.com>
2019-09-25 12:42:11 -0400

    5

--- Turtle.py
+++ Turtle.py
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
+=======
+import turtle
+
+painter=turtle.Turtle()
+
+for _ in range(4):
+    painter.forward(100)
+    painter.left(90)
+
+turtle.done()
+
+>>>>>>> parent of 0402e46... the third turtle
 
