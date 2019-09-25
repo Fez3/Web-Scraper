eaccc70ccd3c5ca72b8287d887e4ee5fde8aed07
Aaron Festinger <aaron.festinger@gmail.com>
2019-09-25 12:22:50 -0400

    trutle 4

--- Turtle.py
+++ Turtle.py
@@ -1,10 +1,15 @@
 import turtle
 
 painter=turtle.Turtle()
+<<<<<<< HEAD
 painter.pensize(15)
 for _ in range(3):
+=======
+
+for _ in range(4):
+>>>>>>> parent of 0402e46... the third turtle
     painter.forward(100)
-    painter.left(120)
+    painter.left(90)
 
 turtle.done()
 
